f48f8bcd6dfee1e6ebada78aed8a1467cb8f2425
Justin Williams <Justintw51@gmail.com>
2025-03-01 19:49:06 -0500

    Added another project called Tournament Form

---
+++ Tournament of Worlds/MatchupEntryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament_of_Worlds
+{
+    class MatchupEntryModel
+    {
+     
+        public TeamModel TeamCompeting { get; set; }
+      
+        public double Score { get; set; }
+      
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TrackerUI/TournamentViewerForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TrackerUI
+{
+    partial class TournamentViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // headerlabel
+            // 
+            this.headerlabel.AutoSize = true;
+            this.headerlabel.Font = new System.Drawing.Font("Comic Sans MS", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerlabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.headerlabel.Location = new System.Drawing.Point(33, 36);
+            this.headerlabel.Name = "headerlabel";
+            this.headerlabel.Size = new System.Drawing.Size(320, 68);
+            this.headerlabel.TabIndex = 0;
+            this.headerlabel.Text = "Tournament:";
+            // 
+            // TournamentViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1733, 1004);
+            this.Controls.Add(this.headerlabel);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "TournamentViewerForm";
+            this.Text = "Tournament Viewer";
+            this.Load += new System.EventHandler(this.TournamentViewerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerlabel;
+    }
+}
+
